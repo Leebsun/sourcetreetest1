294e73f521f9b556ecc5b2f84fbabee5b9fff139
lsb <gkgktmdfl@gmail.com>
2019-04-01 00:04:55 +0900

    플러스마이너스 테스트

--- src/sourcetreetest1/helloworld.java
+++ src/sourcetreetest1/helloworld.java
@@ -2,7 +2,8 @@ package sourcetreetest1;
 
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("hello World!");
+		System.out.println("깃은 너무 편행");
+		System.out.println("헤헤헤헤");
 	}
 
 }
